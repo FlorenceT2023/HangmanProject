ef6d6a33dba136b6593e9660499e760fd3453955
mogopaly87 <mogononso@gmail.com>
2022-07-30 12:53:38 -0230

    seoncd commit

--- src/com/cna/cp2561/Main.java
+++ src/com/cna/cp2561/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("This is a test commit.");
-//        first commit
+
+//        second commit
     }
 }
