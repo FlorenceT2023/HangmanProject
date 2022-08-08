4be99a8996b9988858609e6d4022e31f2791add7
Florence Tat <florence.tat28@ed.cna.nl.ca>
2022-08-07 23:28:17 -0230

    Added ASCII title art in Main Added main menu Gameplay mechanics are in isPlay method Updated word lists for testing

--- src/com/cna/cp2561/Main.java
+++ src/com/cna/cp2561/Main.java
@@ -10,10 +10,10 @@ public class Main {
     public static void main(String[] args) {
 
         // reads title txt file and displays it
-        try(Scanner input = new Scanner(Paths.get("hangmanTitle.txt"))) {
+        try(Scanner title = new Scanner(Paths.get("hangmanTitle.txt"))) {
 
-            while(input.hasNextLine()) {
-                System.out.println(input.nextLine());
+            while(title.hasNextLine()) {
+                System.out.println(title.nextLine());
             }
         } catch (FileNotFoundException | SecurityException | FormatterClosedException ex) {
             ex.printStackTrace();
@@ -24,88 +24,139 @@ public class Main {
         }
         System.out.println();
 
+        // Main menu displays options and starts game on difficulty user chooses
+        Scanner sc = new Scanner(System.in);
+        boolean mainLoop = true;
 
-        Scanner scanner = new Scanner(System.in);
-
-        boolean play = true;
-        while (play)
-        {
-            WordBank.loadWords(1);
-            String randomWord = WordBank.getRandomWord();
-            System.out.printf("The random word is \"%s\"%n", randomWord);
-            String[] randomWordArray = randomWord.split("");
-            ArrayList<String> mainRandomWordArray = new ArrayList<>(Arrays.asList(randomWordArray));
-            int amountOfGuesses = Display.arrDisplay.length;
-
-            ArrayList<String> hashedArray = generateHashedArray(mainRandomWordArray);
+        int menuChoice;
+        while(true){
+            System.out.println("******* Hangman Main Menu *******\n");
+            System.out.println("1. Easy");
+            System.out.println("2. Medium");
+            System.out.println("3. Hard");
+            System.out.println("4. Exit");
             System.out.println();
+            System.out.println("Please enter menu choice: ");
+            menuChoice = sc.nextInt();
 
-            boolean wordIsGuessed = false;
-            int tries = 0;
+            boolean play = true;
+            Scanner scanner = new Scanner(System.in);
 
-            while (!wordIsGuessed & tries != amountOfGuesses)
-            {
-                System.out.println("Current Guesses: ");
-                printArrayContent(hashedArray);
-                System.out.printf("You have %d amount of tries left.\n", (amountOfGuesses)-tries);
-                System.out.println("Enter a single letter: ");
-                String input = scanner.next();
+            switch(menuChoice) {
+                case 1:
+                    // easy mode
+                    while (play)
+                    {
+                        WordBank.loadWords(1);
+                        play = isPlay(true, scanner);
 
-
-                if (input.equals("-"))
-                {
-                    play = false;
-                    wordIsGuessed = true;
+                    }// END of outer while loop
                     System.out.println("Game Over!");
-                }
-
-                else if (!mainRandomWordArray.contains(input)){
-
-                    tries++;
-
-                }
-                else {
-                    for(int i = 0; i < mainRandomWordArray.size(); i++)
+                    break;
+                case 2:
+                    // Medium mode
+                    while (play)
                     {
-                        if(mainRandomWordArray.get(i).equals(input))
-                        {
-                            hashedArray.set(i, input);
-                        }
+                        WordBank.loadWords(2);
+                        play = isPlay(true, scanner);
 
-                    }
-                    if(mainRandomWordArray.equals(hashedArray))
+                    }// END of outer while loop
+                    System.out.println("Game Over!");
+                    break;
+                case 3:
+                    // Hard mode
+                    while (play)
                     {
-                        wordIsGuessed = true;
-                        System.out.println("Congratulations!");
-                        System.out.print("You spelled ");
-                        printArrayContent(hashedArray);
+                        WordBank.loadWords(3);
+                        play = isPlay(true, scanner);
+
+                    }// END of outer while loop
+                    System.out.println("Game Over!");
+                    break;
+                case 4:
+                    System.out.println("Exiting game...thanks for playing!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println();
+            }
+        }
+    }
+
+    // isPlay contains the gameplay mechanics of Hangman
+    private static boolean isPlay(boolean play, Scanner scanner) {
+        String randomWord = WordBank.getRandomWord();
+        System.out.printf("The random word is \"%s\"%n", randomWord);
+        String[] randomWordArray = randomWord.split("");
+        ArrayList<String> mainRandomWordArray = new ArrayList<>(Arrays.asList(randomWordArray));
+        int amountOfGuesses = Display.arrDisplay.length;
+
+        ArrayList<String> hashedArray = generateHashedArray(mainRandomWordArray);
+        System.out.println();
+
+        boolean wordIsGuessed = false;
+        int tries = 0;
+
+        while (!wordIsGuessed & tries != amountOfGuesses)
+        {
+            System.out.println("Current Guesses: ");
+            printArrayContent(hashedArray);
+            System.out.printf("You have %d amount of tries left.\n", (amountOfGuesses)-tries);
+            System.out.println("Enter a single letter: ");
+            String input = scanner.next();
+
+
+            if (input.equals("-"))
+            {
+                play = false;
+                wordIsGuessed = true;
+                System.out.println("Game Over!");
+            }
+
+            else if (!mainRandomWordArray.contains(input)){
+
+                tries++;
+
+            }
+            else {
+                for(int i = 0; i < mainRandomWordArray.size(); i++)
+                {
+                    if(mainRandomWordArray.get(i).equals(input))
+                    {
+                        hashedArray.set(i, input);
                     }
+
                 }
-                if (amountOfGuesses == tries)
+                if(mainRandomWordArray.equals(hashedArray))
                 {
                     wordIsGuessed = true;
+                    System.out.println("Congratulations!");
+                    System.out.print("You spelled ");
+                    printArrayContent(hashedArray);
                 }
-                else {
-                    System.out.println(Display.arrDisplay[tries]);
-                }
-
-
-            }// END of inner while loop
-            if(amountOfGuesses == tries)
+            }
+            if (amountOfGuesses == tries)
             {
-                System.out.println("You ran out of guesses.");
+                wordIsGuessed = true;
             }
-            System.out.println("Would like to play again? (yes/no)");
-            scanner.nextLine();
-            String choice = scanner.nextLine();
-            if(choice.equals("no")){
-                play = false;
-            } else if (choice.equals("yes")) {
-                play = true;
+            else {
+                System.out.println(Display.arrDisplay[tries]);
             }
 
-        }// END of outer while loop
-        System.out.println("Game Over!");
+        }// END of inner while loop
+        if(amountOfGuesses == tries)
+        {
+            System.out.println("You ran out of guesses.");
+        }
+        System.out.println("Would like to play again? (yes/no)");
+        scanner.nextLine();
+        String choice = scanner.nextLine();
+        if(choice.equals("no")){
+            play = false;
+        } else if (choice.equals("yes")) {
+            play = true;
+        }
+        return play;
     }
 
     public static ArrayList<String> generateHashedArray(ArrayList<String> mainArray)
